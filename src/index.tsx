4f07858484e6b55006d92e6fdab97fbdde0f42dc
antrax <antrax2013@hotmail.com>
2022-03-22 21:04:45 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,9 @@ import ReactDOM from 'react-dom';
 import './scss/elements/index.scss';
 import App from './App';
 
-//https://www.search-foresight.com/bonnes-pratiques-seo-frameworks-javascript/
-//https://www.goodmotion.fr/blog/seo-pour-les-web-apps-react-js
-//https://www.gatsbyjs.com/docs/tutorial/
-//Manifeste - https://medium.com/@ryoldash/customize-webpack-config-of-react-app-created-with-create-react-app-7a78c7849edc
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const rootElement = document.getElementById('root');
-
-if (rootElement?.hasChildNodes()) {
-  ReactDOM.hydrate(<App />, rootElement);
-} else {
-  ReactDOM.render(<App />, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
