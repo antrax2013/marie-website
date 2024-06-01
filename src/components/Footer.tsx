1a9bc51953b392c2e3d8c45e5afd0f8fc8028eea
antrax <antrax2013@hotmail.com>
2024-06-01 12:52:43 +0200

    Feat(front) : Add new address + erreur 404

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,32 @@
 const Footer = () => {
-  return (
-    <section className='flex flex-column md:flex-row'>
-      <span className='hidden md:inline-flex m-1'>
-        uniquements sur rendez-vous
-      </span>
-      <span className='hidden md:inline-flex m-1'> - </span>
-      <span className='m-1'>
-        Rdv{' '}
-        <a type='tel' href={`tel:${process.env.REACT_APP_META_NUMTEL}`}>
-          {process.env.REACT_APP_META_NUMTEL}
-        </a>
-        {process.env.REACT_APP_ACTIVERDV == '1' && (
-          <>
-            ou en ligne{' '}
-            <a
-              href={process.env.REACT_APP_URLRDV}
-              rel={'external'}
-              target={'_blank'}
-            >
-              cliquez ici
-            </a>
-          </>
-        )}
-      </span>
-    </section>
-  );
+	return (
+		<section className='flex flex-column md:flex-row'>
+			<span className='hidden md:inline-flex m-1'>
+				uniquements sur rendez-vous
+			</span>
+			<span className='hidden md:inline-flex m-1'> - </span>
+			<span className='m-1'>
+				Rdv{' '}
+				<span itemProp='telephone'>
+					<a type='tel' href={`tel:${process.env.REACT_APP_META_NUMTEL}`}>
+						{process.env.REACT_APP_META_NUMTEL}
+					</a>
+				</span>
+				{process.env.REACT_APP_ACTIVERDV == '1' && (
+					<>
+						ou en ligne{' '}
+						<a
+							href={process.env.REACT_APP_URLRDV}
+							rel={'external'}
+							target={'_blank'}
+						>
+							cliquez ici
+						</a>
+					</>
+				)}
+			</span>
+		</section>
+	);
 };
 
 export default Footer;
